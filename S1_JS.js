61565c8df58e82104c0085c4f8a3f88262ebeb48
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-03-25 21:02:01 +0100

    add let to variable t1

--- S1_JS.js
+++ S1_JS.js
@@ -2,27 +2,27 @@
 
 //exercice 2
 
-t1 = [1,0,2,3,4];
-t2 = [3,5,6,7,8,13];
+// t1 = [1,0,2,3,4];
+// t2 = [3,5,6,7,8,13];
 
 
-function somme(t1,t2){
-  var Somme=[];
-  for (var i = 0; i < t2.length; i++) {
-      Somme[i]=t1[i]+t2[i]
+// function somme(t1,t2){
+//   var Somme=[];
+//   for (var i = 0; i < t2.length; i++) {
+//       Somme[i]=t1[i]+t2[i]
     
-  }
-  return Somme;
-}
+//   }
+//   return Somme;
+// }
 
 
-console.log(somme(t1,t2));
+// console.log(somme(t1,t2));
 
 
 //exercice 1
 
 
-t1 = ['aa','aaaa','aaaaaaa','aaaaaaaaa'];
+let t1 = ['aa','aaaa','aaaaaaa','aaaaaaaaa'];
 
 function lengthS(t1){
     let t2 = t1[0];
