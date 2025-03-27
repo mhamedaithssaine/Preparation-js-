949161febb8ed0f42018f330058dac00320a7657
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-03-27 22:56:14 +0100

    add exercice 3

--- S1_JS.js
+++ S1_JS.js
@@ -22,17 +22,42 @@
 //exercice 1
 
 
-let t1 = ['aa','aaaa','aaaaaaa','aaaaaaaaa'];
+// let t1 = ['aa','aaaa','aaaaaaa','aaaaaaaaa'];
 
-function lengthS(t1){
-    let t2 = t1[0];
-  for (let i = 1; i < t1.length; i++) {
-    if (t1[i].length > t2[i].length) {
-      t2 = t1[i];
-    }
-  }
-  return t2;
-}
+// function lengthS(t1){
+//     let t2 = t1[0];
+//   for (let i = 1; i < t1.length; i++) {
+//     if (t1[i].length > t2[i].length) {
+//       t2 = t1[i];
+//     }
+//   }
+//   return t2;
+// }
 
-console.log(lengthS(t1));
-console.log("".length)
+// console.log(lengthS(t1));
+// console.log("".length)
+
+
+
+
+//exercice 3 
+
+// function removeEle(tab,value){
+//   let result=[];
+//   let found = false;
+
+//   for (let i = 0; i < tab.length-1; i++) {
+//            if(tab[i]===value){
+//             found = true ;
+//            }
+//           if(found){
+//             result[result.length]=tab[i+1];
+//           }
+//         }
+//         return result;
+// }
+// let tab = [1,2,3,8,77,6,9,4,5,2]
+// let result = removeEle(tab,3);
+// console.log(result);
+
+
